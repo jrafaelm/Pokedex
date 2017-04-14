8812a788f7b0b3f24ceffb0b7d06f199223118ef
Rafael Moraes <jrafaelm@gmail.com>
2017-04-14 11:03:17 -0300

    [Issue-1] First setup, adding initial pods, creating initial folders

---
+++ Pokedex/Pokedex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pokedex
+//
+//  Created by Rafael Moraes on 13/04/17.
+//  Copyright © 2017 Rafael Moraes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pokedex/Pokedex/Presentation/Pokemon/PokemonListViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Pokedex
+//
+//  Created by Rafael Moraes on 13/04/17.
+//  Copyright © 2017 Rafael Moraes. All rights reserved.
+//
+
+#import "PokemonListViewController.h"
+
+@interface PokemonListViewController ()
+
+@end
+
+@implementation PokemonListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
